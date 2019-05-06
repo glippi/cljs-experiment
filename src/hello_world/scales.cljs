55b1cdf993cc33be917db7351884fb3a033d6846
Gabriele Lippi <gabriele@lippi.net>
2019-05-06 18:14:13 +0200

    Add maxValue for xScale

--- src/hello_world/scales.cljs
+++ src/hello_world/scales.cljs
@@ -1,13 +1,26 @@
-(ns ^:figwheel-hooks hello-world.scales
-  (:require [cljsjs.d3])
+(ns hello-world.scales
+  (:require
+   [cljsjs.d3]
+   [hello-world.model :as m :refer [model]]
+  )
 )
+
+(def maxValue
+  (->> @model
+      :bars
+      (map :value)
+      (apply max)
+      int
+  ))
+
+(println maxValue)
 
 (defn xScale [x]
   (let [getXCoord
-    (-> js/d3 
+    (-> js/d3
         (.scaleLinear)
-        (.domain #js [0 1000])
-        (.range #js [ 0 500])
+        (.domain #js [0 maxValue])
+        (.range #js [0 500])
       )]
     (getXCoord x)
   )
